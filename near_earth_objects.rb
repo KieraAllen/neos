1bd74c8a991c5648bfffe381a00ea019be4784db
Kiera Allen <kieraaallen@gmail.com>
2020-11-27 07:23:10 -0700

    Create method to connect to api

--- near_earth_objects.rb
+++ near_earth_objects.rb
@@ -8,11 +8,11 @@ Figaro.load
 
 class NearEarthObjects
   def self.find_neos_by_date(date)
-    conn = Faraday.new(
-      url: 'https://api.nasa.gov',
-      params: { start_date: date, api_key: ENV['nasa_api_key']}
-    )
-    asteroids_list_data = conn.get('/neo/rest/v1/feed')
+    # conn = Faraday.new(
+    #   url: 'https://api.nasa.gov',
+    #   params: { start_date: date, api_key: ENV['nasa_api_key']}
+    # )
+    asteroids_list_data = connect_api(date).get('/neo/rest/v1/feed')
 
     parsed_asteroids_data = JSON.parse(asteroids_list_data.body, symbolize_names: true)[:near_earth_objects][:"#{date}"]
 
@@ -34,6 +34,13 @@ class NearEarthObjects
       biggest_astroid: largest_astroid_diameter,
       total_number_of_astroids: total_number_of_astroids
     }
+  end
+
+  def self.connect_api(date)
+    Faraday.new(
+      url: 'https://api.nasa.gov',
+      params: { start_date: date, api_key: ENV['nasa_api_key']}
+    )
   end
 
   def self.diameter(astroid)
